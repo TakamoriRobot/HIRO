a38bf4d551702eedd8ad9bda2a16dccd0fe6b3a6
Joseph Casebeer <joseph@takamorirobot.com>
2026-07-04 20:49:27 -0700

    Commented HIRO_Emotes

--- src/HIRO_Emotes.cpp
+++ src/HIRO_Emotes.cpp
@@ -1,8 +1,31 @@
+/*
+ * File Name: HIRO_Emotes.cpp 
+ * Description: Library for containing the emote motion choreography for H.I.R.O. Model TR-01-A  Unit by Takamori Robot
+ * Author: Joseph Casebeer, Takamori Robot
+ * Date Created: December 31st 2025.
+ * Website: https://www.takamorirobot.com
+ * 
+ * Instructions: The main functions for each emote are what you can use in your sketch. 
+ * The supporting functions are used by the main functions and are not intended to be used in your sketch.
+ * 
+ * 
+ * Choreographed movements 
+ * 1. wave - HIRO waves its right arm
+ */
+
+
 #include <HIRO.h>
 #include <HIRO_Functions.h>
 #include <HIRO_Emotes.h>
 
-void wave(HIRO &hiro){ //Robot raises its arm, and waves
+
+/*
+ * HIRO Emotes Section 1 SubSection 1: HIRO Wave - Main Functions
+ */
+
+void wave(HIRO &hiro){ 
+  //@brief HIRO raises his right arm, counterbalances his leg and waves
+  //@param hiro - The HIRO object to perform the wave emote
   counterBalanceLeg(hiro, 2);
   raiseArmWave(hiro);
   pullRightArmBack(hiro);
@@ -16,8 +39,8 @@ void wave(HIRO &hiro){ //Robot raises its arm, and waves
 
 
 /*
-  Movements for wave();
-*/
+ * HIRO Emotes Section 1 SubSection 2: HIRO Wave - Supporting Functions
+ */
 
 void counterBalanceLeg(HIRO &hiro, int speed){ //Extends the left back leg back so the robot doesn't fall when it waves
   bool rfsb = false; bool rflb = false; bool rbsb = false; bool rblb = false;bool lfsb = false; bool lflb = false; bool lbsb = false; bool lblb = false; 
--- src/HIRO_Emotes.h
+++ src/HIRO_Emotes.h
@@ -1,14 +1,34 @@
+/*
+ * File Name: HIRO_Emotes.h 
+ * Description: Library for containing the emote motion choreography for H.I.R.O. Model TR-01-A  Unit by Takamori Robot
+ * Author: Joseph Casebeer, Takamori Robot
+ * Date Created: December 31st 2025.
+ * Website: https://www.takamorirobot.com
+ * 
+ * Instructions: The main functions for each emote are what you can use in your sketch. 
+ * The supporting functions are used by the main functions and are not intended to be used in your sketch.
+ * 
+ * 
+ * Choreographed movements 
+ * 1. wave - HIRO waves its right arm
+ */
+
+
 #ifndef HIRO_Emotes_h
 #define HIRO_Emotes_h
 
 #include <HIRO.h>
 #include <HIRO_Functions.h>
 
+/*
+ * HIRO Emotes Section 1 SubSection 1: HIRO Wave - Main Functions
+ */
+
 void wave(HIRO &hiro);
 
 /*
-  Movements for wave();
-*/
+ * HIRO Emotes Section 1 SubSection 2: HIRO Wave - Supporting Functions
+ */
 
 void counterBalanceLeg(HIRO &hiro, int speed);
 void pullRightArmBack(HIRO &hiro);
